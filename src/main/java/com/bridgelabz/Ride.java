08e430bab62f210ad24480c6513ffa8aef208b6d
Basila123 <basilanoushad92@gmail.com>
2021-11-26 00:20:42 +0530

    added second use case

---
+++ src/main/java/com/bridgelabz/Ride.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+ class Ride {
+    public final double distance;
+    public final int time;
+
+    public Ride(double distance, int time){
+        this.distance = distance;
+        this.time = time;
+    }
+}
---
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceGeneratorTest{
+    @Test
+    public void givenDistanceAndTimeWhenAddedShouldReturnTotalFare() {
+        InvoiceGenerator iG = new InvoiceGenerator();
+        double distance = 2;
+        int time = 5;
+        double totalFare = iG.calculateFare(distance, time);
+        Assert.assertEquals(25, totalFare, 0.0);
+    }
+    @Test
+    public void givenDistanceAndTimeWhenAddedShouldReturnTotalFareOfMultipleRides(){
+        InvoiceGenerator iG = new InvoiceGenerator();
+        Ride[] rides = {
+                new Ride(2.0,5),
+                new Ride(3.0,5),
+                new Ride(1.0,1)
+        };
+        double multipleRideFare = iG.calculateFare(rides);
+        Assert.assertEquals(71,multipleRideFare,0.0);
+    }
+}
+
